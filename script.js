32d69801fdb646838d24a41fe6bc1eefc3bbe378
Alexandra <alexandrita@ukr.net>
2019-07-08 19:20:44 +0300

    Add 1 file

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 //Функция, используя рекурсию
-/*function powTwo(a) {
+function powTwo1(a) {
 	if(a === 1){
 		return console.log("YES");
 	} else if(a % 2 === 0 && a !== 0){
-		return powTwo(a/2);
+		return powTwo1(a/2);
 	} else console.log("NO");
-}*/
+}
+
+powTwo1(0);
+powTwo1(1);
+powTwo1(2);
+powTwo1(3);
+powTwo1(4);
+powTwo1(35);
+powTwo1(32);
+powTwo1(51);
+powTwo1(128);
 
 //Функция, используя цикл
-function powTwo(a) {
-	while (a !== 0 && a !== 1 && a % 2 === 0){
-	a /= 2;	
+function powTwo2(a) {
+	while (a !== 0 && a % 2 === 0){
+		a /= 2;	
 	}
 	//console.log(a);
-	if (a === 0 || a > 1){
+	if (a !== 1){
 		return console.log("NO");
 	} else {
 		return console.log("YES");
 	}
 }
 
-
-powTwo(0);
-powTwo(1);
-powTwo(2);
-powTwo(3);
-powTwo(4);
-powTwo(35);
-powTwo(32);
-powTwo(51);
-powTwo(128);
+powTwo2(0);
+powTwo2(1);
+powTwo2(2);
+powTwo2(3);
+powTwo2(4);
+powTwo2(35);
+powTwo2(32);
+powTwo2(51);
+powTwo2(128);
